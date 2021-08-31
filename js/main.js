a0b69762f1e3cfa614e93be65f4ad79c0b2f24e2
shimaa3434 <bwmn900@gmail.com>
2021-08-31 20:08:10 +0200

    update

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function openMenu(){
+    document.getElementById('side-menu').style.display= 'block';
+    document.getElementById('menu-btn').style.display= 'none';
+    document.getElementById('close-btn').style.display= 'block';
+};
+function closeMenu(){
+    document.getElementById('side-menu').style.display= 'none';
+    document.getElementById('menu-btn').style.display= 'block';
+    document.getElementById('close-btn').style.display= 'none';
+};
+$(document).ready(function(){
+$('#new-pro').carousel({
+    interval: 1000,
+  });
+});
